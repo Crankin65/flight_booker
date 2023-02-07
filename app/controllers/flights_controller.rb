d384a18bd08e0d39ac00dde704f16c2d636a2cbb
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2023-02-07 00:07:38 -0500

    Created search on flight index page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,25 @@ class FlightsController < ApplicationController
 
   def index
     @flights = Flight.all
+    @airports = Airport.all
+
+    @departure_airport_options = Flight.all.map do  |f|
+      [f.departure_airport.name]
+    end
+
+    @arrival_airport_options = Flight.all.map do  |f|
+      [f.arrival_airport.name]
+    end
+
+    @passengers = [1,2,3,4]
+    @departure_date = Flight.all.map do |f|
+      f.flight_time.to_date
+    end
   end
+
+  private
+    # def airport_flight_params
+    #   params.require(:airport).permit(:)
+    #
+    # end
 end
