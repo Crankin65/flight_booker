f6b824461d6d203e2299209518e977361dd6432e
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2023-02-07 22:38:44 -0500

    Updates to clean code

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,5 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
-    @airports = Airport.all
-
     @departure_airport_options = Airport.all.map {|a| [a.name, a.id]}
     @arrival_airport_options = Airport.all.map {|a| [a.name, a.id] }
     @passengers = [1,2,3,4]
