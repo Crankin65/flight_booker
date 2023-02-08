928733fc5ae6971df8a2353a05261e5f892daacb
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2023-02-07 22:20:45 -0500

    Updated airport search to create matching flights.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,26 +1,26 @@
 class FlightsController < ApplicationController
-
   def index
     @flights = Flight.all
     @airports = Airport.all
 
-    @departure_airport_options = Flight.all.map do  |f|
-      [f.departure_airport.name]
-    end
-
-    @arrival_airport_options = Flight.all.map do  |f|
-      [f.arrival_airport.name]
-    end
-
+    @departure_airport_options = Airport.all.map {|a| [a.name, a.id]}
+    @arrival_airport_options = Airport.all.map {|a| [a.name, a.id] }
     @passengers = [1,2,3,4]
-    @departure_date = Flight.all.map do |f|
-      f.flight_time.to_date
+    @departure_date =  Flight.all.map {|f| [f.flight_time.to_date, f.flight_time]}.uniq
+
+    if params[:commit]
+      #This should be better
+     @searched_flights = Flight.where(departure_airport_id: params[:departure_airport_id],
+                                        arrival_airport_id: params[:arrival_airport_id],
+                                        flight_time: params[:departure_date])
     end
+
+
   end
 
   private
-    # def airport_flight_params
-    #   params.require(:airport).permit(:)
-    #
-    # end
+    def flight_params
+      params.permit(:departure_airport_id, :arrival_airport_id, :passenger_count, :departure_date, :commit)
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,12 @@ Flight.destroy_all
 
 Airport.create!([{name: "LGA"},{name: "JFK"}, {name: "EWR"}])
 Flight.create!([{flight_time: DateTime.new(2022,1,2,3,4,5), flight_duration: 5, departure_airport_id: Airport.where(name:"EWR").take.id, arrival_airport_id: Airport.where(name: "JFK").take.id},
-                {flight_time: DateTime.new(2020,1,2,3,4,5), flight_duration: 4, departure_airport_id: Airport.where(name:"LGA").take.id, arrival_airport_id: Airport.where(name: "EWR").take.id},
-                {flight_time: DateTime.new(2012,1,2,3,4,5), flight_duration: 3, departure_airport_id: Airport.where(name:"JFK").take.id, arrival_airport_id: Airport.where(name: "LGA").take.id} ])
+                {flight_time: DateTime.new(2022,1,2,3,4,5), flight_duration: 4, departure_airport_id: Airport.where(name:"LGA").take.id, arrival_airport_id: Airport.where(name: "EWR").take.id},
+                {flight_time: DateTime.new(2022,1,2,3,4,5), flight_duration: 7, departure_airport_id: Airport.where(name:"LGA").take.id, arrival_airport_id: Airport.where(name: "EWR").take.id},
+                {flight_time: DateTime.new(2022,1,2,3,4,5), flight_duration: 4, departure_airport_id: Airport.where(name:"JFK").take.id, arrival_airport_id: Airport.where(name: "EWR").take.id},
+                {flight_time: DateTime.new(2022,1,2,3,4,5), flight_duration: 4, departure_airport_id: Airport.where(name:"EWR").take.id, arrival_airport_id: Airport.where(name: "LGA").take.id},
+                {flight_time: DateTime.new(2022,1,2,3,4,5), flight_duration: 4, departure_airport_id: Airport.where(name:"LGA").take.id, arrival_airport_id: Airport.where(name: "JFK").take.id},
+                {flight_time: DateTime.new(2022,1,2,3,4,5), flight_duration: 3, departure_airport_id: Airport.where(name:"JFK").take.id, arrival_airport_id: Airport.where(name: "LGA").take.id} ])
 
 p "Created #{Airport.count} airports and #{Flight.count} flights(s)"
 
