94cffae113b8dfe61824f087289a6634db8d7547
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2023-02-10 12:21:52 -0500

    Created booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,13 +2,22 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @passenger = Passenger.new
     @flight = Flight.find(params[:flight_id])
     @passenger_count = params[:passenger_count]
+  end
+
+  def create
+
   end
 
   private
 
   def booking_params
-    params.permit(:departure_airport_id, :arrival_airport_id, :passenger_count, :departure_date, :flight_id, :commit)
+    params.permit(:departure_airport_id, :arrival_airport_id,
+                  :passenger_count, :departure_date, :flight_id, :commit)
+  end
+
+  def create_booking_params
   end
 end
