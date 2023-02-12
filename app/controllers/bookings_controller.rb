1df38f1d8779c9fa2e2f378eb98336cd07ef1599
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2023-02-12 01:58:57 -0500

    Completed project

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,16 +9,30 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @passenger = Passenger.new(booking_params[:passengers])
+    @flight = Flight.find(params[:flight_id])
+    @booking = @flight.bookings.build(booking_params)
+
+    if @booking.save
+      flash[:notice] = "Passenger save was successsful"
+      redirect_to booking_path(@flight)
+    else
+      flash[:alert] = "Passenger not saved"
+    end
+
+  end
+
+  def show
+    @flight = Flight.find(params[:id])
+    @booking = @flight.bookings.last
+    @passengers = @booking.passengers
 
   end
 
   private
 
   def booking_params
-    params.permit(:departure_airport_id, :arrival_airport_id,
-                  :passenger_count, :departure_date, :flight_id, :commit)
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:first_name, :last_name, :email])
   end
 
-  def create_booking_params
-  end
 end
